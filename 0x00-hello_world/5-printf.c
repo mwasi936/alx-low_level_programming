596414fbed43171330a36b8359864794aaea48ec
mwasi936 <mwasijeff9363gmail.com>
2023-09-07 16:52:47 -0700

    3

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0 when succesfull
+ **/
+
+int main(void)
+{
+  printf("with proper grammar, but the outcomeis a pace of art");
+  return (0);
+}
