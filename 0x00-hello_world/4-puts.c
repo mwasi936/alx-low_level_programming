9f72df41c2323d266ea1b7f84233a341bb1ff318
mwasi936 <mwasijeff9363gmail.com>
2023-09-07 17:07:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - prints a string
- *Return: always 0
- */
+*main - prints a string
+*Return: always 0
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
