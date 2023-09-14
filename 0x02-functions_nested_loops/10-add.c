47d45e7858f8ccbcf66f8e4823499ad06868a916
mwasi936 <mwasijeff9363gmail.com>
2023-09-14 02:16:05 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
-#include 'main.h"
+#include "main.h"
 /**
- *add - function that sums two numbers
- *@a: parameter 1
- *@b: parameter 2
- *Return: a + b
- */
-int add9int a, int b)
+*add - function that sums two numbers
+*@a: parameter 1
+*@b: parameter 2
+*Return: a + b
+*/
+int add(int a, int b)
 {
 	return (a + b);
 }
