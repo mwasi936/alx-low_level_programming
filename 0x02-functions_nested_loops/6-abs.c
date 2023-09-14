273e451b65aec803062430525524ae98457e33fc
mwasi936 <mwasijeff9363gmail.com>
2023-09-13 18:28:14 -0700

    7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - function to find the absolutr value of number
+ * @a:function parameter
+ * Return: -a or a
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
+	{
+		return (a);
+	}
+	return (0);
+}
