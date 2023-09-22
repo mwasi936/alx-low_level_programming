b501f44271ec80d6dbc841cb7f91f8d238019f84
mwasi936 <mwasijeff9363gmail.com>
2023-09-21 20:10:32 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capita;ized
+ * Return: capitalised string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sp[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		for (j = 0 ; j < 13 ; j++)
+		{
+			if (s[i] == sp[j])
+			{
+				if  (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
