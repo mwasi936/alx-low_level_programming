e970177d3ca00dcbd1e815c5ca9e21daab417083
mwasi936 <mwasijeff9363gmail.com>
2023-10-10 09:12:10 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: longer description
+ */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+#endif
