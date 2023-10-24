7a10caf0122737ddfdcc62693a3be3de3b752799
mwasi936 <mwasijeff936@gmail.com>
2023-10-24 11:02:33 -0700

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t print_list(const list_t *h)
 	elements = 0;
 	while (h != NULL)
 	{
-	       if	(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-	       else
-		       printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		elements++;
 	}
