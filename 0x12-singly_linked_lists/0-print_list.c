0073acebf3df940db45b711820527a702758588d
mwasi936 <mwasijeff936@gmail.com>
2023-10-24 10:57:23 -0700

    lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints lists
+ * @h: pointer to struct
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+	       if	(h->str == NULL)
+			printf("[0] (nil)\n");
+	       else
+		       printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
