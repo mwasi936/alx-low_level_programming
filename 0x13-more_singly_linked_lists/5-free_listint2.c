01dfd5ff87acd58ac0a64c747303f58fd6f10e79
mwasi936 <mwasijeff936@gmail.com>
2023-10-26 12:46:33 -0700

    well freet=d it and headed it to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 /**
  * free_listint2 - frees list and sets head to NULL
  * @head: head of node
- * Return: null
+ * Return: NULL
  */
 
 void free_listint2(listint_t **head)
@@ -20,5 +20,4 @@ void free_listint2(listint_t **head)
 	}
 
 	*head = NULL;
-	return;
 }
