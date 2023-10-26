e119d3c133a892154124388c47f98e203e249e0e
mwasi936 <mwasijeff936@gmail.com>
2023-10-26 14:05:41 -0700

    head node of list gone, daamn

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include  "lists.h"
+
+/**
+ * pop_listint - deletes head node returns headnode data n
+ * @head:head of node
+ * Return: head node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	int a = 0;
+
+	if (head != NULL && *head != NULL)
+	{
+		listint_t *temp = *head;
+		temp->n = a;
+		*head = (*head)->next;
+
+		free(temp);
+	}
+	
+	return (a);
+}
