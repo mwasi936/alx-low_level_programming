83fefe2df23a24818cfd78f135a8d759fbbbf93d
mwasi936 <mwasijeff936@gmail.com>
2023-10-26 12:25:27 -0700

    well freet=d it and headed it to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list and sets head to NULL
+ * @head: head of node
+ * Return: null
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+
+		*head = (*head)->next;
+
+		free(temp);
+	}
+	*head = NULL;
+}
