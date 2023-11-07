c5ace75af6b2922d0fae709df7a726ba836782dc
mwasi936 <mwasijeff936@gmail.com>
2023-11-07 13:52:31 -0800

    bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int copy = 1UL << index;
 
-	if (index >= sizeof(unsigned long int) *8)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
