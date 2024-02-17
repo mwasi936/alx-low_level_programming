bd604fd1a6e07284854ca6c20adc936a297fe4dc
mwasi936 <mwasijeff936@gmail.com>
2024-02-17 09:25:37 -0800

    file io ou

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,42 +9,37 @@
 char *create_buffer(void);
 void close_file(int fd);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int from, to;
     ssize_t r, w;
     char *buffer;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+    if (argc != 3) {
+        fprintf(stderr, "Usage: cp file_from file_to\n");
         exit(97);
     }
 
     buffer = create_buffer();
 
     from = open(argv[1], O_RDONLY);
-    if (from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+    if (from == -1) {
+        perror("Error opening source file");
         free(buffer);
         exit(98);
     }
 
     to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+    if (to == -1) {
+        perror("Error opening destination file");
         free(buffer);
+        close_file(from);
         exit(99);
     }
 
-    while ((r = read(from, buffer, BUFFER_SIZE)) > 0)
-    {
+    while ((r = read(from, buffer, BUFFER_SIZE)) > 0) {
         w = write(to, buffer, r);
-        if (w == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        if (w == -1) {
+            perror("Error writing to destination file");
             free(buffer);
             close_file(from);
             close_file(to);
@@ -52,9 +47,8 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (r == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+    if (r == -1) {
+        perror("Error reading from source file");
         free(buffer);
         close_file(from);
         close_file(to);
@@ -65,26 +59,21 @@ int main(int argc, char *argv[])
     close_file(from);
     close_file(to);
 
-    return (0);
+    return 0;
 }
 
-char *create_buffer(void)
-{
+char *create_buffer(void) {
     char *buffer = malloc(BUFFER_SIZE);
-    if (buffer == NULL)
-    {
+    if (buffer == NULL) {
         perror("Error allocating buffer");
-        exit(EXIT_FAILURE);
+        exit(99);
     }
     return buffer;
 }
 
-void close_file(int fd)
-{
-    if (close(fd) == -1)
-    {
+void close_file(int fd) {
+    if (close(fd) == -1) {
         perror("Error closing file descriptor");
-        exit(EXIT_FAILURE);
+        exit(100);
     }
 }
-
